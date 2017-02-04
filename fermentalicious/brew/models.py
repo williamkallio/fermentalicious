4c4ebcb2597349fafc59fbf03ea9898986444fa0
William Kallio <wkallio@gmail.com>
2017-02-04 10:40:31 -0800

    initial data model

--- fermentalicious/brew/models.py
+++ fermentalicious/brew/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Beverage(models.Model):
+    beverage_name = models.CharField(max_length=200)
+    beverage_description = models.CharField(max_length=1000)
+    create_datetime = models.DateTimeField('datetime created')
+
+class Fermentation(models.Model):
+    beverage = models.ForeignKey(Beverage, on_delete=models.CASCADE)
+    fermentation_start_date = models.DateTimeField('datetime started')
+    fermentation_end_date = models.DateTimeField('datetime ended')
+    fermentation_original_gravity = models.DecimalField()
+    fermentation_final_gravity = models.DecimalField()
+    fermentation_final_abv = models.DecimalField()
+
+class FermentationProgress(models.Model):
+    fermentation = models.ForeignKey(Fermentation, on_delete=models.CASCADE)
+    measurement_date = models.DateTimeField('datetime measured')
+    specific_gravity = models.DecimalField()
+    temperature = models.DecimalField()
+    
+    
+    
+    
+
